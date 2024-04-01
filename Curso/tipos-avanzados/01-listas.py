8191f6829293ec6170793bfe78156cae560d3f18
sebatucco <c.sebastian.ojeda@gmail.com>
2024-04-01 16:51:50 -0300

    listas 1

---
+++ Curso/tipos-avanzados/01-listas.py
@@ -0,0 +1,12 @@
+numeros = [1, 2, 3]
+letras = ["a", "b", "c"]
+palabras = ["uno", "dos", "tres"]   # lista de listas
+boleans = [True, False, True]
+matriz = [[0, 1], [2, 3]]
+ceros_y_unos = [0, 1] * 10
+ceros = [0] * 10
+alfanumericos = numeros + letras
+rango = list(range(0, 10))
+chars = list("hola mundo")
+
+print(chars)
